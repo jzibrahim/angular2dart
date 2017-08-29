39c6549bcf16ad978a424b45db20fa57214be762
Jennie <jzibrahim@gmail.com>
2017-08-28 22:24:46 -0700

    Rename a few directories and remove some redundant ones.

--- GameShow/C_6_service/web/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:angular2/platform/browser.dart';
-
-import 'package:game/game.dart';
-
-void main() {
-  bootstrap(GameComponent);
-}
--- GameShow/C_7/lib/card.dart
+++
@@ -1,9 +0,0 @@
-enum Level { EASY, HARD }
-
-class Card {
-  String question;
-  List<String> options;
-  int answer;
-
-  Card(this.question, this.options, this.answer);
-}
